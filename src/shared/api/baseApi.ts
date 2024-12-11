c095729c4dce07ecafd18388e87e6b7ae9612f52
Muhammad <muxammad_205@mail.ru>
2024-12-11 11:34:54 +0500

    fix(redirect): Redirect login when 401

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,22 +1,42 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryApi,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 
-import { getCookie } from "@shared/lib/helpers";
+import { deleteCookie, getCookie } from "@shared/lib/helpers";
 import { RootState } from "@shared/types/store";
 
 const baseUrl = import.meta.env.VITE_APP_BASE_URL;
 
-const baseQuery = fetchBaseQuery({
-  baseUrl,
+const baseQuery = async (
+  args: string | FetchArgs,
+  api: BaseQueryApi,
+  extraOptions: Record<string, unknown>,
+) => {
+  const rawResult = await fetchBaseQuery({
+    baseUrl,
 
-  prepareHeaders: (headers, { getState }) => {
-    const token =
-      (getState() as RootState).auth?.token || getCookie("access_token");
-    if (token) {
-      headers.set("authorization", `Bearer ${token}`);
-    }
-    return headers;
-  },
-});
+    prepareHeaders: (headers, { getState }) => {
+      const token =
+        (getState() as RootState).auth?.token || getCookie("access_token");
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  })(args, api, extraOptions);
+
+  // Check for 401 Unauthorized error
+  if (rawResult.error?.status === 401) {
+    // Redirect to the login page
+    deleteCookie("access_token");
+    window.location.href = "/login";
+  }
+
+  return rawResult;
+};
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
