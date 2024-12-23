e228d2e885f800b2242d2e24293e826eeb8812cf
Muhammad <muxammad_205@mail.ru>
2024-12-23 18:33:53 +0500

    feat(QX1009-106): QX1009-106 is started

--- src/widgets/org-edit-third-step/ui/index.tsx
+++ src/widgets/org-edit-third-step/ui/index.tsx
@@ -13,34 +13,34 @@ import {
 } from "antd";
 import { AnyObject } from "antd/es/_util/type";
 import { CheckboxChangeEvent } from "antd/es/checkbox";
-import { t } from "i18next";
+import i18next, { t } from "i18next";
 import { FC, useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 
+import { useGetPhoneTypeQuery } from "@entities/phone";
+
+import { GET_ALL_ACTIVE_STATUS } from "@shared/lib/helpers";
 import { RootState } from "@shared/types";
 
 import { setData } from "../model/Slicer";
 
-// MOCKS
-const mocks = {
-  phoneTypeOptions: [
-    {
-      id: 1,
-      key: 1,
-      label: "Тип телефона 1",
-      value: "phone-type 1",
-      "phone-type": "Тип телефона 1",
-    },
-    {
-      id: 2,
-      key: 2,
-      label: "Тип телефона 2",
-      value: "phone-type 2",
-      "phone-type": "Тип телефона 2",
-    },
-  ],
-  columns: [
+export const OrgEditThirdStepUI: FC = () => {
+  const { data } = useSelector(
+    ({ useEditOrgThirdStepSlice }: RootState) => useEditOrgThirdStepSlice,
+  );
+  const dispatch = useDispatch();
+  const { data: phoneTypesData, isLoading: phoneTypesLoading } =
+    useGetPhoneTypeQuery({
+      all: GET_ALL_ACTIVE_STATUS.all,
+      status: GET_ALL_ACTIVE_STATUS.active,
+    });
+  const [selectedPhoneType, setSelectedPhoneType] = useState<AnyObject[] | []>(
+    [],
+  );
+  const [phone, setPhone] = useState<string>("");
+
+  const columns = [
     {
       title: t("phone-type"),
       dataIndex: "phone-type",
@@ -51,23 +51,6 @@ const mocks = {
       dataIndex: "phone",
       key: "phone",
     },
-  ],
-};
-
-export const OrgEditThirdStepUI: FC = () => {
-  const { data } = useSelector(
-    ({ useEditOrgThirdStepSlice }: RootState) => useEditOrgThirdStepSlice,
-  );
-  const dispatch = useDispatch();
-  const [selectedPhoneType, setSelectedPhoneType] = useState<AnyObject[] | []>(
-    [],
-  );
-  const [phoneTypeOptions, setPhoneTypeOptions] = useState<AnyObject[] | []>(
-    mocks.phoneTypeOptions,
-  );
-  const [phone, setPhone] = useState<string>("");
-
-  const columns = [
     {
       width: 80,
       title: t("secret"),
@@ -80,7 +63,6 @@ export const OrgEditThirdStepUI: FC = () => {
         />
       ),
     },
-    ...mocks.columns,
     {
       title: t("action"),
       dataIndex: "action",
@@ -106,9 +88,9 @@ export const OrgEditThirdStepUI: FC = () => {
   const onSecretCheck = (e: CheckboxChangeEvent, record: AnyObject) => {
     const filteredData = data
       ?.filter((item: { phone: string }) => item.phone === record.phone)
-      .map((item: { secret: boolean }) => ({
+      .map((item: { isSecret: boolean }) => ({
         ...item,
-        secret: e.target.checked,
+        isSecret: e.target.checked,
       }));
 
     const otherData = data?.filter(
@@ -137,8 +119,11 @@ export const OrgEditThirdStepUI: FC = () => {
     setPhone("");
   };
 
-  const onSelectType = (value: string, option: AnyObject | unknown) => {
-    setSelectedPhoneType([option as AnyObject]);
+  const onSelectType = (
+    value: string,
+    option: { value: string | number; label: string },
+  ) => {
+    setSelectedPhoneType([{ phoneTypeId: value, "phone-type": option.label }]);
   };
 
   return (
@@ -148,7 +133,7 @@ export const OrgEditThirdStepUI: FC = () => {
           <Form.Item name={"account"} label={t("account")}>
             <Input placeholder={t("account")} />
           </Form.Item>
-          <Form.Item name={"email"} label={t("email")}>
+          <Form.Item name={"mail"} label={t("email")}>
             <Input placeholder={t("email")} />
           </Form.Item>
           <Form.Item name={"index"} label={t("index")}>
@@ -156,7 +141,7 @@ export const OrgEditThirdStepUI: FC = () => {
           </Form.Item>
         </Col>
         <Col span={12}>
-          <Form.Item name={"tin"} label={t("tin")}>
+          <Form.Item name={"inn"} label={t("tin")}>
             <Input placeholder={t("tin")} />
           </Form.Item>
           <Form.Item name={"bank_number"} label={t("bank_number")}>
@@ -172,11 +157,16 @@ export const OrgEditThirdStepUI: FC = () => {
           <Flex align="center" gap={8}>
             <label htmlFor="phone-type">{t("phone-type")}</label>
             <Select
+              loading={phoneTypesLoading}
+              allowClear
               showSearch
               id="phone-type"
-              value={selectedPhoneType[0]?.value}
+              value={selectedPhoneType[0]?.phoneTypeId}
               onSelect={onSelectType}
-              options={phoneTypeOptions}
+              options={phoneTypesData?.data?.map((item: AnyObject) => ({
+                value: item.id,
+                label: item.name[i18next.language],
+              }))}
               style={{ flex: 1 }}
             />
           </Flex>
